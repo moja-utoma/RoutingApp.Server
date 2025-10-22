7b0b2f47105dd236c2c7300c2490ebf7e19f32f1
moja-utoma <ryabova.yelizaveta@gmail.com>
2025-10-22 15:29:20 +0300

    Added .yml file

--- RoutingApp.API/Program.cs
+++ RoutingApp.API/Program.cs
@@ -21,8 +21,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
@@ -108,11 +108,11 @@ var app = builder.Build();
 app.UseCors("AllowAngular");
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
@@ -132,6 +132,9 @@ app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapGet("/api/ping", () => Results.Ok("Pong! No auth required."))
+   .AllowAnonymous();
 
 app.MapControllers();
 
