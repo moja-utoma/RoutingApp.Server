19336a44594ad43faad11e791600903cfd6df9de
moja-utoma <ryabova.yelizaveta@gmail.com>
2025-10-27 14:50:59 +0200

    Changed calc route to soft deletable

---
+++ RoutingApp.API/Migrations/20251027125024_Soft delete for calc route.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoutingApp.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Softdeleteforcalcroute : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeletedAt",
+                table: "CalculatedRoutes",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "CalculatedRoutes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "CalculatedRoutes");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "CalculatedRoutes");
+        }
+    }
+}
