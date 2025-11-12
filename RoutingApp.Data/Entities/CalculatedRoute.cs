b3535d5ad7806c91072d5e7e42011a6a900994ce
moja-utoma <ryabova.yelizaveta@gmail.com>
2025-11-12 12:30:22 +0200

    Added new migration

--- RoutingApp.Data/Entities/CalculatedRoute.cs
+++ RoutingApp.Data/Entities/CalculatedRoute.cs
@@ -1,12 +1,10 @@
 ﻿namespace RoutingApp.Data.Entities
 {
-    public class CalculatedRoute : ISoftDeletable
+    public class CalculatedRoute
     {
         public int Id { get; set; }
         public string Calculation { get; set; }
         public DateTime CreatedAt { get; set; }
         public Route Route { get; set; }
-        public bool IsDeleted { get; set; }
-        public DateTimeOffset? DeletedAt { get; set; }
     }
 }
